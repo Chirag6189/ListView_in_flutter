19f238bf1cde57bd0401fe75882648f48c0bad65
Chirag Jadhav <jadhavchirag487@gmail.com>
2024-02-16 12:33:40 +0530

    add assets folder and add some images for profile img

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class My_app extends StatefulWidget {
 class _My_appState extends State<My_app> {
   @override
   Widget build(BuildContext context) {
+    var profile_img = ['assets/images/p1.jpg','assets/images/p2.jpg','assets/images/p3.jpg','assets/images/p4.jpg','assets/images/p5.jpeg','assets/images/p6.jpeg','assets/images/p7.jpg','assets/images/p8.jpg','assets/images/p9.jpg','assets/images/p10.jpg'];
     var user_name = ["Chirag Jadhav","John Doe","Mike Johnson","Emily Davis","Jane Smith","Alex Turner","Sophie Wilson","Olivia Brown","Charlie Parker","Grace Miller"];
     var sub_tital = ["Java Developer","Software Developer","Marketing Specialist","Data Scientist","Graphic Designer","UX/UI Designer","Project Manager","Human Resources Manager","Mobile App Developer","Content Writer"];
     return MaterialApp(
